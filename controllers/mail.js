66634eb55234327fa55aaf73048330bfdbac7977
Nwankwo Chinedum <54178355+calculusky@users.noreply.github.com>
2020-06-30 23:38:18 +0100

    Update mail.js

--- controllers/mail.js
+++ controllers/mail.js
@@ -3,7 +3,7 @@ const sendgrid = require('nodemailer-sendgrid-transport');
 
 const api = {
     auth: {
-        api_key: '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'
+        
     }
 }
 
